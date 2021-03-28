f320cff3ac9d8891b7323eb4fb8af25dab3dca54
annynaweb <sousa.annyc@gmail.com>
2021-03-28 19:31:46 -0300

    Pop Up Logica

---
+++ Assets/Scrips/Quests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Quests : MonoBehaviour
+{
+    public GameObject PopUp;
+    public GameObject ItemQuest;
+    public bool ativaQuest;
+    void Start()
+    {
+        ativaQuest = false;
+    }
+    void Update()
+    {
+        if(ativaQuest){
+            PopUp.SetActive(true);
+            ItemQuest.SetActive(false);
+        }else{
+            PopUp.SetActive(false);
+            ItemQuest.SetActive(true);
+        }
+        
+    }
+    public void AtivaPopUp(){
+        ativaQuest = true;
+    }
+    public void DesativaPopUp(){
+        ativaQuest = false;
+    }
+}
