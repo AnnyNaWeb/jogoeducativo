c71b24fdffb8d6628f1b336cdaf759064c5a4e38
annynaweb <sousa.annyc@gmail.com>
2021-05-24 00:15:30 -0300

    Lista Suspensa

--- Assets/Scrips/Quests.cs
+++ Assets/Scrips/Quests.cs
@@ -2,30 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Quests : MonoBehaviour
-{
+public class Quests : MonoBehaviour {
     public GameObject PopUp;
-    public GameObject ItemQuest;
+    // public GameObject ItemQuest;
     public bool ativaQuest;
-    void Start()
-    {
+    void Start () {
         ativaQuest = false;
     }
-    void Update()
-    {
-        if(ativaQuest){
-            PopUp.SetActive(true);
-            ItemQuest.SetActive(false);
-        }else{
-            PopUp.SetActive(false);
-            ItemQuest.SetActive(true);
+    void Update () {
+        if (ativaQuest) {
+            PopUp.SetActive (true);
+            //    ItemQuest.SetActive(false);
+        } else {
+            PopUp.SetActive (false);
+            //   ItemQuest.SetActive(true);
         }
-        
+
     }
-    public void AtivaPopUp(){
+    public void AtivaPopUp () {
         ativaQuest = true;
     }
-    public void DesativaPopUp(){
+    public void DesativaPopUp () {
         ativaQuest = false;
     }
 }
