7a994abcd9e28fb672d18d107d21a6a039365096
annynaweb <sousa.annyc@gmail.com>
2021-05-23 22:26:57 -0300

    Move Player e Joystick Touch

---
+++ Assets/Scrips/MovePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    public float playerSpeed, playerJumpForce;
+
+    private Vector3 direction;
+    private VariableJoystick joystick;
+    void Start () {
+        joystick = GameObject.Find ("Variable Joystick").GetComponent<VariableJoystick> ();
+    }
+
+    void Update () {
+        MoveMobile ();
+        Rotation ();
+    }
+
+    void MoveMobile () {
+        direction = (Vector3.forward * joystick.Vertical) + (Vector3.right * joystick.Horizontal);
+    }
+    void Rotation () {
+        if (direction != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation (direction);
+        }
+        transform.Translate (direction * (playerSpeed * Time.deltaTime), Space.World);
+    }
+}
